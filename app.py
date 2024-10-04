146fdd52dc32a2e14301eeed201ed968e993ae7f
Inkflow <tomcucherosset@hotmail.fr>
2024-10-04 19:57:44 +0200

    Deuxième test du downloader YouTube

--- app.py
+++ app.py
@@ -1,27 +1,40 @@
 import os
 from pytube import YouTube
 
-# Downlaod the video
-def download(url):
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'outtmpl': '%(id)s.%(ext)s',
-        'restrictfilenames': True,
-        'noplaylist': True,
-        'quiet': True,
-        'logtostderr': False,
-        'progress_hooks': [my_hook]
-    }
-    with YouTube(url) as yt:
-        yt.streams.first().download(ydl_opts)
+# On télécharge une vidéo selon le lien fourni
+def download(link):
+    # On crée un objet YouTube
+    yt = YouTube(link)
+    # On récupère le titre de la vidéo
+    title = yt.title
+    # On récupère le lien de la vidéo
+    link = yt.streams.filter(progressive=True).first().url
+    # On récupère le nom du fichier
+    filename = title.replace(' ', '_')
+    # On récupère le nom du fichier avec l'extension
+    filename = filename + '.mp4'
+    # On récupère le chemin du fichier
+    path = os.path.join(os.getcwd(), filename)
+    # On télécharge la vidéo
+    yt.streams.filter(progressive=True).first().download(path)
+    return filename
 
-# Hook function to print download progress
-def my_hook(d):
-    if d['status'] == 'finished':
-        print()
-        print('Done downloading')
-        print()
+# On télécharge une vidéo selon son ID
+def download_id(id):
+    # On crée un objet YouTube
+    yt = YouTube(id)
+    # On récupère le titre de la vidéo
+    title = yt.title
+    # On récupère le lien de la vidéo
+    link = yt.streams.filter(progressive=True).first().url
+    # On récupère le nom du fichier
+    filename = title.replace(' ', '_')
+    # On récupère le nom du fichier avec l'extension
+    filename = filename + '.mp4'
+    # On récupère le chemin du fichier
+    path = os.path.join(os.getcwd(), filename)
+    # On télécharge la vidéo
+    yt.streams.filter(progressive=True).first().download(path)
+    return filename
 
-if __name__ == '__main__':
-    url = input('Enter the URL of the video: ')
-    download(url)
+download('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
